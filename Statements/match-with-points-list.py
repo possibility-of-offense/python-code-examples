4de5444c5acbd639a65a1a5374303c8628bfc84f
possibility-of-offense <73138031+possibility-of-offense@users.noreply.github.com>
2022-05-26 09:32:48 +0300

    Update Math statements with list of points! Traversing lists with dictionaries
    Added case with rest* parameters and concatenate the points into a string; used ternary operator!

--- Statements/match-with-points-list.py
+++ Statements/match-with-points-list.py
@@ -1,4 +1,4 @@
-points = [[1, 2], [1, 3], [1, 10], [3, 12]]
+points = [[1, 2], [1, 3], [1, 10], [3, 12], [5, 32]]
 # points = [[1, 5]]
 
 match points :
@@ -10,13 +10,27 @@ match points :
         print(f'Two points with coordinates of {y1} and {y2} on y axis')
     case [[1, y1], [1, y2], [1, y3]] :
         print(f'Three points with coordinates of {y1}, {y2} and {y3} on y axis')
+    case [[1, y1], [1, y2], [1, y3], *rest] :
+        out = 'The leftover point is with coordinate of '
+        for p in range(len(rest)) :
+            comma = ', ' if p < len(rest) -1 else ''
+            out += str(rest[p][0]) + ' ' + str(rest[p][1]) + comma
+
+            if(p == len(rest) - 1) :
+                out += '!'
+    # this case is important if you don't have the rest parameter in the previous case
+    # case _ :
+    #     points_on_y = []
+
+    #     for point in range(len(points)) :
+    #         if points[point][0] != 1 :
+    #             continue
+    #         points_on_y.append({'index': point + 1, 'point': points[point]})
+
+    #     for point in points_on_y : 
+    #         print('The point {ind} is on the y axis with coordinate of {point}'.format(ind = point['index'], point = point['point'][1]))
+
     case _ :
-        points_on_y = []
+        print('Something else')
 
-        for point in range(len(points)) :
-            if points[point][0] != 1 :
-                continue
-            points_on_y.append({'index': point + 1, 'point': points[point]})
-
-        for point in points_on_y : 
-            print('The point {ind} is on the y axis with coordinate of {point}'.format(ind = point['index'], point = point['point'][1]))
+print(out)
