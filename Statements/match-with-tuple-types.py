a6da0b56d8956eb07f9670d5b579f83e10d24f3d
possibility-of-offense <73138031+possibility-of-offense@users.noreply.github.com>
2022-05-26 09:05:19 +0300

    Math statements with tuple types and extracting values into variables

---
+++ Statements/match-with-tuple-types.py
@@ -0,0 +1,17 @@
+tuple_type = ('petko', 'georgiev')
+greeting = ''
+
+def uppercase_first(v) :
+    out = v[0].upper() + v[1:]
+    return out
+
+match tuple_type :
+    case (x, 'iliev') :
+        if x == 'ventsi' :
+            greeting = 'Hello {name} Iliev!'.format(name = uppercase_first(x))
+    case (x, y) :
+        greeting = 'Hello {first} {last}! You\'ve come to a first time in here!'.format(first = uppercase_first(x), last = uppercase_first(y))
+    case _ :
+        print('The name is not recognized')
+
+print(greeting)
