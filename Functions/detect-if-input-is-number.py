d68181bb24ed1fc0a4ae62b61a7db550902c146f
possibility-of-offense <73138031+possibility-of-offense@users.noreply.github.com>
2022-05-30 13:45:05 +0300

    Added min / max checking

--- Functions/detect-if-input-is-number.py
+++ Functions/detect-if-input-is-number.py
@@ -1,6 +1,8 @@
 def detect_done() :
     count = 0
     total = 0
+    min = None
+    max = None
 
     while True :
         inp = input('Enter a number: ')
@@ -11,6 +13,15 @@ def detect_done() :
         try :
             inp_parset_to_int = int(inp)
 
+            if min is None and max is None :
+                min = inp_parset_to_int
+                max = inp_parset_to_int
+            else :
+                if inp_parset_to_int < min :
+                    min = inp_parset_to_int
+                elif inp_parset_to_int > max :
+                    max = inp_parset_to_int
+
             count = count + 1
             total = total + inp_parset_to_int
         except :
@@ -20,5 +31,7 @@ def detect_done() :
     print(f'Total {total}')
     print(f'Count {count}')
     print('Avg {cond}'.format(cond = total / count if total > 0 else 'No total and count yet!'))
+    print(f'Max {max}')
+    print(f'Min {min}')
 
 detect_done()
