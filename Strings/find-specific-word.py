8d5c9212920bea96a9302d5404a2b32930f95d61
possibility-of-offense <73138031+possibility-of-offense@users.noreply.github.com>
2022-05-30 15:32:09 +0300

    Update when there are multiple whitespaces before the first character appears

--- Strings/find-specific-word.py
+++ Strings/find-specific-word.py
@@ -1,4 +1,4 @@
-word = 'This is some word sent by this email blah@blah.bg on some date in the future'
+word = 'This is some word sent by this email blah@      blah.bg on some date in the future'
 
 def find_word(w) :
     start_pos = w.find('@')
@@ -8,13 +8,22 @@ def find_word(w) :
         print('Not found')
         exit()
 
+    found_char = False
+
     for ind in range(len(w[start_pos:])) :
         char = w[start_pos + 1:][ind]
-        
-        if char == ' ':
-            break
+
+        if char == ' ' and found_char == False:
+            found_char = False
+            continue
         else :
-            email += char
+            found_char = True
+        
+        if found_char == True :
+            if char == ' ':
+                break
+            else :
+                email += char
     
     print(f'The email is {email}!')
 
