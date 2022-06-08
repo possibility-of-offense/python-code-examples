6a8f82576960ab912b9f4cda2939b1de27e1d13f
possibility-of-offense <73138031+possibility-of-offense@users.noreply.github.com>
2022-06-08 12:03:46 +0300

    Example with operations on list (+ and *)

---
+++ Lists/list-operations.py
@@ -0,0 +1,14 @@
+# The + operator concatenates lists:
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+[1, 2, 3, 4, 5, 6]
+# Similarly, the * operator repeats a list a given number of times:
+
+[0] * 4
+[0, 0, 0, 0]
+[1, 2, 3] * 3
+[1, 2, 3, 1, 2, 3, 1, 2, 3]
+# # The first example repeats four times. The second example repeats the list three times.
