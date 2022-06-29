020dec963c8eac9c312d303b365026a8e267425a
possibility-of-offense <73138031+possibility-of-offense@users.noreply.github.com>
2022-06-29 16:02:38 +0300

    Update

--- Classes/operation.py
+++ Classes/operation.py
@@ -1,5 +1,18 @@
-class Operation:
-    stuff = list()
+class Counting:
+    line_counts = 0
+
+    def __init__(self):
+        print('I have a child class')
+
+    def increase_line_counts(self, n):
+        self.line_counts = n
+
+# Operation class
+
+class Operation(Counting):
+    def __init__(self):
+        self.stuff = list()
+        super().__init__()
 
     def operate(self, handler, to_put):
         with open(handler, encoding="utf-8") as f:
@@ -7,6 +20,7 @@ class Operation:
 
             for line in read_data.split('\n'):
                 if line.startswith('From zqian'):
+                    self.increase_line_counts(self.line_counts + 1)
                     self.stuff.append(line)
                 else:
                     continue
@@ -14,7 +28,8 @@ class Operation:
             with open(to_put, 'w', encoding="utf-8") as p:
                 p.write('\n'.join(self.stuff))
 
+                print(self.line_counts)
+
 
 reading = Operation()
-
 reading.operate('../mailbox.txt', '../froms.txt')
